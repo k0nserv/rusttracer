a145bbbc127cbe4a7650ba07d63fcd43d4c2d65b
Hugo Tunius <h@tunius.se>
2017-09-17 14:31:23 +0300

    Implement command line flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 extern crate time;
+extern crate getopts;
 
 extern crate rusttracer;
 
@@ -7,21 +8,45 @@ use std::path::Path;
 use std::env;
 use std::f64::consts::PI;
 
+use getopts::Options;
+
 use rusttracer::math::{Matrix4, Point3, Transform, Vector3};
 use rusttracer::{Camera, Color, Config, MaterialTemplate, Renderer, Scene, SuperSampling};
 use rusttracer::geometry::{Intersectable, Plane, Transformable};
 use rusttracer::lights::PointLight;
 use rusttracer::mesh_loader::MeshLoader;
 
-const CONFIG: &str = "LowQuality.toml";
+const DEFAULT_CONFIG_PATH: &str = "default.toml";
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn main() {
-    let benchmark = match env::var("BENCHMARK") {
-        Ok(s) => s.parse::<bool>().unwrap_or(false),
-        Err(_) => false,
+    let args: Vec<String> = env::args().collect();
+
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optopt("c", "config-path", "config file path, uses `default.toml` if not specified", "CONFIG_PATH");
+    opts.optflag("b", "benchmark", "Benchmark by rendering the scene multiple times");
+    opts.optflag("h", "help", "prints this help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
     };
 
-    let config = Config::new_from_file(CONFIG).expect("Invalid configuration");
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let benchmark = matches.opt_present("b");
+
+    let config_path = matches.opt_str("c").unwrap_or(DEFAULT_CONFIG_PATH.to_string());
+
+    let config = Config::new_from_file(&config_path).expect("Invalid configuration");
 
     let template = MaterialTemplate::new(
         Color::blue() * 0.02,
